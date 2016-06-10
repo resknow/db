f70230ba05ed4a376974d30ab24f3c66f21b58d5
Chris <chris@chrismade.me>
2016-06-10 14:23:55 +0100

    Add db/config filter

--- functions.php
+++ functions.php
@@ -3,6 +3,18 @@
 require_once __DIR__ . '/vendor/medoo.php';
 require_once __DIR__ . '/classes/class.database.php';
 
+/**
+ * Filter: db/config
+ * 
+ * @NOTE This filter is applied VERY early in
+ * the bootstrapping process. In order to use it,
+ * your plugin name must begin with letters 0-9, a-c
+ * and filters must be applied from your plugin
+ * functions.php file.
+ */
+$_config['db'] = apply_filters( 'db/config', $_config['db'] );
+
+// Create Database instance
 $_db = new Database($_config);
 
 /**
